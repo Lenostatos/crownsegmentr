c6f5106b4a6629c254864b7c05e409f2fcd75766
Leon Steinmeier <Lenostatos@gmx.de>
2020-04-17 13:07:07 +0200

    Install packages needed for development.

--- package_development_code.R
+++ package_development_code.R
@@ -3,3 +3,11 @@ install.packages("remotes")
 remotes::install_github("rstudio/renv")
 renv::init()
 
+# Install packages needed for development
+install.packages(c("roxygen2", "testthat", "knitr"))
+remotes::install_github("r-lib/devtools")
+remotes::install_github("r-lib/usethis")
+install.packages("styler")
+
+# Install packages that this package depends on
+install.packages("BH")
