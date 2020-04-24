31f9d945efc6109a7d3f2b5e43a41ac6b7eac3a0
Leon Steinmeier <Lenostatos@gmx.de>
2020-04-24 21:00:09 +0200

    Further document the package development process.

--- package_development_code.R
+++ package_development_code.R
@@ -9,7 +9,40 @@ remotes::install_github("r-lib/devtools")
 remotes::install_github("r-lib/usethis")
 install.packages("styler")
 
-# Install packages that this package depends on
-install.packages("BH")
+# Make usethis functions available on the command line without package prefix.
+# usethis::use_usethis()
 
+# Create bare-bones package structure
 usethis::create_package(".")
+
+# Make usethis functions available on the command line without package prefix.
+# usethis::use_usethis()
+
+# Ignore this file
+usethis::use_build_ignore("package_development_code.R")
+
+# Add a license
+usethis::use_gpl3_license(name = "Leon Steinmeier")
+
+# Install packages that this package uses
+usethis::use_package("assertthat")
+
+install.packages("dbscan")
+usethis::use_package("dbscan")
+
+install.packages("data.table")
+usethis::use_package("data.table")
+
+usethis::use_package("BH", type = "LinkingTo")
+
+usethis::use_rcpp()
+usethis::use_r(name = "crownsegmentr-package.R")
+
+devtools::document()
+# Or use keyboard shortcut Ctrl + Shift + D
+
+usethis::use_r(name = "try_to_extract_coordinate_data")
+# Execute usethis::use_test() interactively when the newly created R file is
+# active in order to create an R file with tests.
+
+usethis::use_r(name = "segment_tree_crowns")
