5ee4734d11e9fd67bd272367b3708c9da06f0de0
Leon Steinmeier <Lenostatos@gmx.de>
2020-05-21 17:38:11 +0200

    Refactor package development code.

--- package_development_code.R
+++ package_development_code.R
@@ -9,14 +9,8 @@ remotes::install_github("r-lib/devtools")
 remotes::install_github("r-lib/usethis")
 install.packages("styler")
 
-# Make usethis functions available on the command line without package prefix.
-# usethis::use_usethis()
-
 # Create bare-bones package structure
 usethis::create_package(".")
-
-# Make usethis functions available on the command line without package prefix.
-# usethis::use_usethis()
 
 # Ignore this file
 usethis::use_build_ignore("package_development_code.R")
@@ -37,14 +31,24 @@ usethis::use_package("BH", type = "LinkingTo")
 
 usethis::use_rcpp()
 usethis::use_r(name = "crownsegmentr-package.R")
+
 # The following stackoverflow post provides valuable info on the Makevars file:
 # https://stackoverflow.com/questions/43597632/understanding-the-contents-of-the-makevars-file-in-r-macros-variables-r-ma
 
-devtools::document()
-# Or use keyboard shortcut Ctrl + Shift + D
+usethis::use_r(name = "try_to_extract_coordinate_data.R")
 
-usethis::use_r(name = "try_to_extract_coordinate_data")
-# Execute usethis::use_test() interactively when the newly created R file is
-# active in order to create an R file with tests.
+usethis::use_r(name = "segment_tree_crowns.R")
 
-usethis::use_r(name = "segment_tree_crowns")
+# Make usethis functions available on the command line without package prefix.
+# usethis::use_usethis()
+
+# Use X after doing Y:
+# + Use devtools::load_all() or Ctrl+Shift+L after changing R code to reinstall
+#   package
+# + Use "Install and restart" in the Build tab or Ctrl+Shift+B after changing
+#   C/C++ code in order to do perform an installation with source compilation.
+# + Use devtools::document() or Ctrl+Shift+D after changing documentation.
+# + Use usethis::use_test() when editing an R source file in order to create a
+#   test file (or switch to it if it already exists).
+# + Use usethis::use_r() when editing a test file in order to switch back to the
+#   respective R source file.
