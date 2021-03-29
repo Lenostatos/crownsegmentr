ee4669cb91ff85931026b547d640d2801c9b618c
Leon Steinmeier <lenostatos@gmx.de>
2021-03-29 14:57:17 +0200

    Add another code example to the documentation showing some parameter variations.

--- R/examples/segment_tree_crowns_examples.R
+++ R/examples/segment_tree_crowns_examples.R
@@ -3,6 +3,7 @@ point_cloud <- lidR::readLAS(system.file(
   "extdata", "MixedConifer.laz", package = "lidR"
 ))
 
+
 # Segment the point cloud
 segmented_point_cloud <- crownsegmentr::segment_tree_crowns(
   point_cloud,
@@ -29,6 +30,7 @@ lidR::plot(
   axis = TRUE
 )
 
+
 # Observe how adjacent crowns are undersegmented when the
 # crown_diameter_to_tree_height parameter is set too high
 segmented_point_cloud <- crownsegmentr::segment_tree_crowns(
@@ -49,6 +51,7 @@ lidR::plot(
   size = 3,
   axis = TRUE
 )
+
 
 # In order to get a more in-depth impression of how the segmentation works,
 # also return and plot modes and centroids.
@@ -81,5 +84,30 @@ lidR::plot(
   color = "crown_id",
   colorPalette = crown_colors,
   size = 1,
+  axis = TRUE
+)
+
+
+# See how the processing gets faster and segmentation accuracy worse when using
+# a larger distance at which centroids are considered to have converged and a
+# larger DBSCAN neighborhood.
+segmented_point_cloud <- crownsegmentr::segment_tree_crowns(
+  point_cloud,
+  crown_diameter_to_tree_height = 0.5,
+  crown_height_to_tree_height = 0.5,
+  centroid_convergence_distance = 0.02,
+  dbscan_neighborhood_radius = 0.5
+)
+
+lidR::plot(
+  segmented_point_cloud,
+  color = "crown_id",
+  colorPalette = c(
+    "#DDDDDD",
+    lidR::random.colors(
+      n = length(unique(segmented_point_cloud@data$crown_id)) - 1
+    )
+  ),
+  size = 3,
   axis = TRUE
 )
