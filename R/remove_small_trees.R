57acd18dee19624e3495d202d04ea62eb51450ac
phyrrros <timon.miesner@gmail.com>
2025-10-09 20:04:08 +0200

    Tests and generic S4 Framework for remove_small_trees

--- R/remove_small_trees.R
+++ R/remove_small_trees.R
@@ -1,57 +1,145 @@
 # Remove small trees
 #' remove small trees
 #'
+
+#'
+#'
+#'
+
+# Generic S4 Function -------------------------------------------------
+
+#' Remove small clusters from segmented point cloud
+#'
 #' The function takes a point cloud in which trees were segmented, and removes
 #' tree clusters that are smaller than a certain radius or a certain height
 #'
-#' @param las a point cloud as lidR LAS object, with a data column "crown_id"
+#' @param point_cloud a point cloud, either as data.frame/data.table, as
+#' lidR::LAS object, or as LasCatalog. Needs to have a data column with the
+#' name
 #' @param min_radius (num) the threshold for crown radius, below which trees
 #' will be removed
 #' @param min_height (num) the threshold for crown height, below which trees
 #' will be removed. Works only if las is normalized.
+#' @param crown_id_column_name the name of the column in which the id of the
+#' crown is saved
 #' @return lidR LAS
 #' @section Details
-#' returns the same las object that was given as input, but with 
+#' returns the same las object that was given as input, but with
 #' altered crown id's. Trees that are considered too small have their crown id
-#' set to NA, and all other crown id's are re-assigned so that they are 
+#' set to NA, and all other crown id's are re-assigned so that they are
 #' without gaps
-#' @export 
+#' @export
+methods::setGeneric("remove_small_trees",
+                    function(point_cloud,
+                             min_radius = 1,
+                             min_height = -Inf,
+                             crown_id_column_name = "crown_id") {
+                      standardGeneric("remove_small_trees")
+                    },
+                    signature = "point_cloud"
+)
 
-remove_small_trees <- function(las, min_radius = 1, min_height = -Inf){
-  # assumes that "las" is a las object with "crown_id" as a data column
+
+
+# Method for data.frames and data.tables ----------------------------------
+#' @describeIn remove_small_trees removes small tree clusters in a segmented
+#'   [LAS object][lidR::LAS-class].
+#'
+#'
+#' @importClassesFrom lidR LAS
+methods::setMethod(
+  "remove_small_trees",
+  signature(point_cloud = "data.frame"),
+  function(point_cloud,
+           min_radius,
+           min_height,
+           crown_id_column_name){
+
+    # TODO: test if it contains a data column of the required name
+
+    # copy point cloud to a reduced data frame that can be used for crown_polygons
+    point_cloud_reduced_df <- point_cloud[,c("X", "Y", "Z")]
+    point_cloud_reduced_df$crown_id <- point_cloud[[crown_id_column_name]]
+
+    # create crown polgons
+    crown.polygons <- crown_polygons_crown_id(point_cloud_reduced_df)
+
+    # identify ids of small crowns
+    small.ids <- crown.polygons[(crown.polygons$radius < min_radius)|
+                                  (crown.polygons$height < min_height),]$crown_id
+
+    # identify for which points the crown is too small
+    crown_is_small <- point_cloud[[crown_id_column_name]] %in% small.ids
+
+    # set crown_id of small trees to NA
+    point_cloud[[crown_id_column_name]][crown_is_small] <- NA
+
+    # create new ids in ascending order without gaps (ugly method, but works)
+    point_cloud[[crown_id_column_name]] <-
+      as.integer(as.factor(point_cloud[[crown_id_column_name]]))
+
+    return(point_cloud)
+
+  })
+
+
+
+# Method for lidR::LAS Objects ----------------------------------------
+#' @describeIn remove_small_trees removes small tree clusters in a segmented
+#'   [LAS object][lidR::LAS-class].
+#'
+#'
+#' @importClassesFrom lidR LAS
+methods::setMethod(
+  "remove_small_trees",
+  signature(point_cloud = "LAS"),
+  function(point_cloud,
+           min_radius,
+           min_height,
+           crown_id_column_name){
+
+  # TODO: test if it contains a data column of the required name
+
+  # copy point cloud to a reduced data frame that can be used for crown_polygons
+  point_cloud_reduced_df <- point_cloud@data[,c("X", "Y", "Z")]
+  point_cloud_reduced_df$crown_id <- point_cloud@data[[crown_id_column_name]]
 
   # create crown polgons
-  crown.polygons <- crown_polygons_crown_id(las)
+  crown.polygons <- crown_polygons_crown_id(point_cloud_reduced_df)
 
   # identify ids of small crowns
   small.ids <- crown.polygons[(crown.polygons$radius < min_radius)|
                                 (crown.polygons$height < min_height),]$crown_id
 
+  # identify for which points the crown is too small
+  crown_is_small <- point_cloud@data[[crown_id_column_name]] %in% small.ids
+
   # set crown_id of small trees to NA
-  las@data$crown_id[las@data$crown_id %in% small.ids] <- NA
+  point_cloud@data[[crown_id_column_name]][crown_is_small] <- NA
 
   # create new ids in ascending order without gaps
-  las@data$crown_id <- as.integer(as.factor(las@data$crown_id))
+  point_cloud@data[[crown_id_column_name]] <-
+    as.integer(as.factor(point_cloud@data[[crown_id_column_name]]))
 
-  return(las)
+  return(point_cloud)
 
-}
+})
 
+
+# TODO: Add case for LAS Catalog
 
 
 
 # Helper function
 
-crown_polygons_crown_id <- function(las){
+crown_polygons_crown_id <- function(point_cloud){
   # takes a normalized and segmented point cloud with a column "crown_id"
   # and returns a SpatVector object with the convex hull of each tree as polygon
 
-  # create data frame with points
-  points.df <- las@data[,c("X","Y", "Z", "crown_id")]
   # remove NA crown_id
-  points.df <- points.df[!is.na(points.df$crown_id)]
+  point_cloud <- point_cloud[!is.na(point_cloud$crown_id),]
   # convert into terra vector object
-  points.vect <- vect(points.df, geom=c("X", "Y"))
+  points.vect <- terra::vect(point_cloud, geom=c("X", "Y"))
 
   # calculate convex hull for every tree
   crowns <- terra::hull(points.vect, by = "crown_id")
@@ -60,10 +148,12 @@ crown_polygons_crown_id <- function(las){
   crowns$area <- suppressWarnings( terra::expanse(crowns) )
   crowns$radius <- sqrt(crowns$area) / 2
   # add Z as the highest Z value in the cluster
-  crowns <- merge(crowns, aggregate(Z ~ crown_id, points.df, max), by = "crown_id")
+  crowns <- merge(crowns, aggregate(Z ~ crown_id, point_cloud, max), by = "crown_id")
   # rename column
   names(crowns)[names(crowns)=="Z"] <- "height"
 
   return(crowns)
 }
+
+
 
---
+++ tests/testthat/test-remove_small_trees.R
@@ -0,0 +1,65 @@
+#  test-remove_small_trees
+
+test_that("The function works on a point cloud data table",{
+  mini_point_cloud <- data.frame(X = c(1,1,2,3,3,4,4,5),
+                                 Y = c(1,5,3,1,5,3,5,5),
+                                 Z = c(8,8,8,8,8,8,8,8),
+                                 crown_id = c(1, NA, 3,3,3,3,4,4))
+
+  processed_point_cloud <- remove_small_trees(mini_point_cloud)
+
+  expect_true(is.na(processed_point_cloud[1,"crown_id"]))
+  expect_equal(processed_point_cloud[5,"crown_id"], 1)
+
+  # vary min_height
+  processed_point_cloud <- remove_small_trees(mini_point_cloud,
+                                              min_height = 10)
+
+  expect_equal(sum(is.na(processed_point_cloud$crown_id)),
+               nrow(mini_point_cloud))
+
+  # Vary min_radius
+  processed_point_cloud <- remove_small_trees(mini_point_cloud,
+                                              min_radius = 5,
+                                              min_height = 3)
+
+  expect_equal(sum(is.na(processed_point_cloud$crown_id)),
+               nrow(mini_point_cloud))
+
+  # vary crown_id_column_name
+  names(mini_point_cloud) <- c("X", "Y", "Z", "foo")
+  processed_point_cloud <- remove_small_trees(mini_point_cloud,
+                                              crown_id_column_name = "foo")
+  expect_true(is.na(processed_point_cloud[1,"foo"]))
+})
+
+
+
+
+test_that("Function works with real-world LAS file",{
+  # read and segment a point cloud for use in the function
+
+  test_point_cloud_file_path <- system.file(
+    "extdata", "MixedConifer.laz",
+    package = "lidR"
+  )
+  test_point_cloud_header <- lidR::readLASheader(test_point_cloud_file_path)
+  test_point_cloud <- lidR::clip_rectangle(
+    lidR::readLAS(test_point_cloud_file_path),
+    xleft = test_point_cloud_header@PHB$`Min X`,
+    ybottom = test_point_cloud_header@PHB$`Min Y`,
+    xright = test_point_cloud_header@PHB$`Min X` + 30,
+    ytop = test_point_cloud_header@PHB$`Min Y` + 30
+  )
+  # segment point cloud as raw material
+  segmented_las <- segment_tree_crowns(test_point_cloud,
+                                       kernel_diameter_slope = 0.2,
+                                       kernel_height_slope = 0.6,
+                                       kernel_diameter_intercept = 2,
+                                       kernel_height_intercept = 1)
+
+  processed_las <- remove_small_trees(segmented_las)
+
+  expect_true(length(unique(processed_las@data$crown_id)) <
+                length(unique(segmented_las@data$crown_id)))
+})
